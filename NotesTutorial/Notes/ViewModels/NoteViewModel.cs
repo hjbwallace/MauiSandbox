83bb0bbfb9a29b620040cb2a788b7f5b56cc6760
Harrison Wallace <hjbwallace@gmail.com>
2023-09-06 22:12:23 +0800

    Adding the NoteViewModel

---
+++ NotesTutorial/Notes/ViewModels/NoteViewModel.cs
@@ -0,0 +1,76 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Notes.Models;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+public class NoteViewModel : ObservableObject, IQueryAttributable
+{
+    private Note _note;
+
+    public NoteViewModel() : this(new())
+    {
+    }
+
+    public NoteViewModel(Note note)
+    {
+        _note = note;
+        SaveCommand = new AsyncRelayCommand(Save);
+        DeleteCommand = new AsyncRelayCommand(Delete);
+    }
+
+    public string Text
+    {
+        get => _note.Text;
+        set
+        {
+            if (_note.Text != value)
+            {
+                _note.Text = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public DateTime Date => _note.Date;
+
+    public string Identifier => _note.Filename;
+
+    public ICommand SaveCommand { get; private set; }
+    public ICommand DeleteCommand { get; private set; }
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("load"))
+        {
+            _note = Note.Load(query["load"].ToString());
+            RefreshProperties();
+        }
+    }
+
+    public void Reload()
+    {
+        _note = Note.Load(_note.Filename);
+        RefreshProperties();
+    }
+
+    private async Task Save()
+    {
+        _note.Date = DateTime.Now;
+        _note.Save();
+        await Shell.Current.GoToAsync($"..?saved={_note.Filename}");
+    }
+
+    private async Task Delete()
+    {
+        _note.Delete();
+        await Shell.Current.GoToAsync($"..?deleted={_note.Filename}");
+    }
+
+    private void RefreshProperties()
+    {
+        OnPropertyChanged(nameof(Text));
+        OnPropertyChanged(nameof(Date));
+    }
+}
--- NotesTutorial/Notes/Views/NotePage.xaml.cs
+++ NotesTutorial/Notes/Views/NotePage.xaml.cs
@@ -1,54 +1,9 @@
 namespace Notes.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
     public NotePage()
     {
         InitializeComponent();
-
-        var appDataPath = FileSystem.AppDataDirectory;
-        var randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
-    }
-
-    public string ItemId
-    {
-        set => LoadNote(value);
-    }
-
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-        {
-            // Delete the file.
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private void LoadNote(string fileName)
-    {
-        var noteModel = new Models.Note();
-        noteModel.Filename = fileName;
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
     }
 }
